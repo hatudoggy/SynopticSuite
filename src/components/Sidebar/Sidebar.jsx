04f880bfc0e7ac4779be101e17eeb1125c4618e7
Morfuseee <markvalenzuela.coding@gmail.com>
2023-06-11 17:29:42 +0800

    UPDATE: Sidebar is semi-responsive
    - Made sidebar responsive
    - Refactored the unordered list
    - Fixed the layout for other devices

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -8,73 +8,82 @@ import Navigation from "./Navigation";
 
 function Sidebar() {
   return (
-    <div className="flex min-h-full w-16 flex-col sm:w-72">
-      <div className="flex h-24 items-center justify-center rounded-tr-2xl bg-primary-logo text-center text-xs font-semibold text-white sm:pb-1.5 sm:text-3xl">
+    <div className="flex h-full max-h-full w-16 flex-col sm:w-72">
+      <div className="flex h-24 items-center justify-center bg-primary-logo text-center text-xs font-semibold text-white sm:pb-1.5 sm:text-3xl">
         Synoptic Suite
       </div>
-      <div className="flex flex-auto flex-col items-center gap-10 rounded-br-2xl bg-primary px-12 py-7">
-        <div className="flex flex-col gap-6">
+      <div className="flex flex-auto flex-col items-center gap-5 bg-primary sm:px-12 py-7 md:gap-7 lg:gap-10">
+        <div className="flex flex-col items-center sm:items-start gap-7 sm:gap-3 md:gap-4 lg:gap-6 sm:w-full w-5/6">
           <Navigation
             // Don't modify this unless you want to adjust
             // the margin, padding, or the gap between the flex items
             classOfWrapper={"sm:gap-7"}
             image={calendar}
-            classOfImage={"sm:w-9 invert-to-white"}
             nameOfNav={"Calendar"}
-            classOfNav={"invisible sm:visible"}
+            classOfNav={"hidden sm:block"}
           />
           <Navigation
             classOfWrapper={"sm:gap-6"}
             image={resources}
-            classOfImage={"sm:w-9 invert-to-white-second"}
             nameOfNav={"Resources"}
-            classOfNav={"invisible sm:visible"}
+            classOfNav={"hidden sm:block"}
           />
           <Navigation
             classOfWrapper={"sm:gap-7"}
             image={clock}
-            classOfImage={"sm:w-9 invert-to-white-second"}
             nameOfNav={"Clock"}
-            classOfNav={"invisible sm:visible"}
+            classOfNav={"hidden sm:block"}
           />
           <Navigation
             classOfWrapper={"sm:gap-7"}
             image={planner}
-            classOfImage={"sm:w-9 invert-to-white-second"}
             nameOfNav={"Planner"}
-            classOfNav={"invisible sm:visible"}
+            classOfNav={"hidden sm:block"}
           />
           <Navigation
             classOfWrapper={"sm:gap-7"}
             image={analytics}
-            classOfImage={"sm:w-9 invert-to-white-second"}
             nameOfNav={"Analytics"}
-            classOfNav={"invisible sm:visible"}
+            classOfNav={"hidden sm:block"}
           />
         </div>
-        <div className="flex flex-auto flex-col gap-4">
-          <div className="ml-2 flex flex-row items-center border-t-2 font-sans text-lg text-white opacity-70 sm:w-40 sm:gap-8"></div>
+        <div className="flex flex-auto flex-col gap-2 md:gap-4 lg:gap-6">
+          <div className="ml-2 hidden flex-row items-center border-t-2 font-sans text-lg text-white opacity-70 sm:flex sm:w-40 sm:gap-8"></div>
           <Navigation
-            classOfWrapper={"sm:gap-3 px-3"}
+            classOfWrapper={"sm:gap-3 px-3 hidden"}
             image={addButton}
-            classOfImage={"sm:w-9 invert-to-white-second"}
             nameOfNav={"Add new link"}
-            classOfNav={"invisible sm:visible text-base font-medium"}
+            classOfNav={"hidden sm:block text-base font-medium"}
           />
           <div className="flex flex-row items-center font-sans text-lg text-white sm:gap-6">
             <ul className="flex list-disc flex-col gap-3 pl-10 font-semibold underline underline-offset-4">
-              <li className="marker:text-green-500 hover:cursor-pointer">
-                STI Education Services Group
-              </li>
-              <li className="marker:text-blue-500 hover:cursor-pointer">
-                One STI Student Portal
-              </li>
+              <NewLinks
+                bulletColor={"marker:text-green-500"}
+                linkName={"STI Education Services Group"}
+              />
+              <NewLinks
+                bulletColor={"marker:text-blue-500"}
+                linkName={"One STI Student Portal"}
+              />
             </ul>
           </div>
-          <div className="ml-2 flex flex-row items-center border-b-2 font-sans text-lg text-white opacity-70 sm:w-40 sm:gap-8"></div>
+          <div className="ml-2 hidden flex-row items-center border-b-2 font-sans text-lg text-white opacity-70 sm:flex sm:w-40 sm:gap-8"></div>
         </div>
       </div>
     </div>
+  );
+}
+
+function NewLinks({ linkName, bulletColor }) {
+  return (
+    <li
+      className={
+        "sm:list-item hidden hover:cursor-pointer " +
+        (bulletColor ? bulletColor : "")
+      }
+    >
+      {linkName}
+    </li>
   );
 }
 
