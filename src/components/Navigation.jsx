4a8f2a3638b0693d65188653391aa2266e814c69
Hatdoggy <topers1820@gmail.com>
2023-06-12 14:53:01 +0800

    - Fixed navigation changing the styling of Sidebar

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,18 +1,25 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function Navigation({ image, nameOfNav, classOfNav, classOfWrapper, link }) {
+  const navigate = useNavigate()
+  
   return (
-    <Link to={link} >
+    
+    
       <div
         className={
           "sm:flex flex-row items-center rounded-lg sm:px-5 w-full py-2 font-sans text-lg text-white hover:cursor-pointer hover:bg-hover-color " +
           (classOfWrapper ? classOfWrapper : "sm:gap-8")
-        }
+        } onClick={()=>{navigate(link)}}
       >
+        
         <img src={image} alt="calendar" className="sm:w-9 w-10 m-auto sm:m-0" />
         <p className={classOfNav}>{nameOfNav}</p>
+        
       </div>
-    </Link>
+   
+
+    
   );
 }
 
