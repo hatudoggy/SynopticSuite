aa56544f9ab62370743b0259ced62ae86c51c260
Morfuseee <markvalenzuela.coding@gmail.com>
2023-07-23 14:51:56 +0800

    UPDATE: Added custom screen sizes and heights within tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 const colors = require("tailwindcss/colors");
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{jsx,css}",
-  ],
+  content: ["./index.html", "./src/**/*.{jsx,css}"],
   theme: {
     extend: {
       colors: {
@@ -23,12 +20,34 @@ export default {
         cyan: colors.cyan,
         orange: colors.orange,
         neutral: colors.gray,
-        'primary': "#3C3E41",
-        'primary-logo': '#447BCD',
-        'hover-color' : '#909090',
+        primary: "#3C3E41",
+        "primary-logo": "#447BCD",
+        "hover-color": "#909090",
       },
     },
-  },
-  plugins: [],
-}
+    screens: {
+      /**** WIDTH ****/
 
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      ml: "900px",
+      // => @media (min-width: 900px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+
+      /**** HEIGHT ****/
+      tall: { raw: "(min-height: 652px)" },
+    },
+  },
+};
